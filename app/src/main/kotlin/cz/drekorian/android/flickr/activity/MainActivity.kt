9fe4cff5fd5df8389a6b624cba6c5a4f34da38a5
Marek Osvald <marek.osvald@greencode.cz>
2024-11-12 09:48:50 +0100

    Update status bar coloring to the EdgeToEdge API.

--- app/src/main/kotlin/cz/drekorian/android/flickr/activity/MainActivity.kt
+++ app/src/main/kotlin/cz/drekorian/android/flickr/activity/MainActivity.kt
@@ -3,6 +3,10 @@ package cz.drekorian.android.flickr.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 import cz.drekorian.android.flickr.ui.FlickrApp
 import cz.drekorian.android.flickr.ui.theme.FlickrTheme
 
@@ -11,9 +15,14 @@ class MainActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             FlickrTheme {
-                FlickrApp()
+                Box(
+                    modifier = Modifier.safeDrawingPadding(),
+                ) {
+                    FlickrApp()
+                }
             }
         }
     }
