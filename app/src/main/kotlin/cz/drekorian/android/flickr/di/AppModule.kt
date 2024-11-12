0899bd96e091394f097245de8719f7abaaba9fbd
Marek Osvald <marek.osvald@greencode.cz>
2024-11-12 08:15:18 +0100

    Migrate to Koin constructor DSL.

--- app/src/main/kotlin/cz/drekorian/android/flickr/di/AppModule.kt
+++ app/src/main/kotlin/cz/drekorian/android/flickr/di/AppModule.kt
@@ -4,14 +4,14 @@ import cz.drekorian.android.flickr.domain.SettingsLocalDataSource
 import cz.drekorian.android.flickr.ui.controls.TopAppBarViewModel
 import cz.drekorian.android.flickr.ui.controls.latest.LatestViewModel
 import cz.drekorian.android.flickr.ui.controls.search.SearchViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
 
     single { SettingsLocalDataSource(get()) }
 
-    viewModel { LatestViewModel(get(), get(), get()) }
-    viewModel { SearchViewModel(get(), get(), get()) }
-    viewModel { TopAppBarViewModel(get()) }
+    viewModelOf(::LatestViewModel)
+    viewModelOf(::SearchViewModel)
+    viewModelOf(::TopAppBarViewModel)
 }
